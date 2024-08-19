64768815ac0050131fa7a8e3b2cef2670088453d
devlee <whffkaos147@naver.com>
2024-08-19 17:47:00 +0900

    test

--- myui5app/webapp/controller/App.controller.js
+++ myui5app/webapp/controller/App.controller.js
@@ -1,14 +1,14 @@
 sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function(BaseController) {
-      "use strict";
-  
-      return BaseController.extend("myui5app.controller.App", {
-        onInit: function() {
-        }
-      });
-    }
-  );
-  
+  [
+    "sap/ui/core/mvc/Controller"
+  ],
+  function (BaseController) {
+    "use strict";
+
+    return BaseController.extend("myui5app.controller.App", {
+      onInit: function () {
+        console.log("Hello World");
+      }
+    });
+  }
+);
