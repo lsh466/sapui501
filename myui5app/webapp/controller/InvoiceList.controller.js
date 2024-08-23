a95b1d21233c2e1bfec9ca59a2dd085b012f9bab
devlee <whffkaos147@naver.com>
2024-08-23 14:01:02 +0900

    Filtering Sorting Grouping

--- myui5app/webapp/controller/InvoiceList.controller.js
+++ myui5app/webapp/controller/InvoiceList.controller.js
@@ -3,8 +3,10 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "../model/formatters",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
   ],
-  function (Controller, JSONModel, formatters) {
+  function (Controller, JSONModel, formatters, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("myui5app.controller.InvoiceList", {
@@ -15,6 +17,20 @@ sap.ui.define(
         });
 
         this.getView().setModel(oViewModel, "view");
+      },
+
+      onFilterInvoices(oEvent) {
+        const aFilter = [];
+        const sQuery = oEvent.getParameter("query");
+        if (sQuery) {
+          aFilter.push;
+          new Filter("ProductName", FilterOperator.Contains, sQuery)();
+          aFilter.push(new Filter("Status", FilterOperator.Contains, sQuery));
+        }
+
+        const oList = this.byId("invoiceList");
+        const oBinding = oList.getBinding("items");
+        oBinding.filter(aFilter);
       },
     });
   }
